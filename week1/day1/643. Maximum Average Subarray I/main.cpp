4d28892b1ab286b4c2c5f4d1be5675ec0509e9b7
ArhanCrane <123109947+ArhanCrane@users.noreply.github.com>
2026-06-02 13:43:12 +0300

    Update main.cpp

--- week1/day1/643. Maximum Average Subarray I/main.cpp
+++ week1/day1/643. Maximum Average Subarray I/main.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
+    //Fixed Size Sliding Window
     double findMaxAverage(vector<int>& nums, int k) {
-        
+        const int n = static_cast<int>(nums.size());
+
+        double addition{};
+        for (int i{}; i < k; i++) {addition += nums[i];}// k <= n, O (k)
+
+        double max_average{addition / k}; // k != 0
+
+        int l{};
+        for (int r{k}; r < n; r++) { // O (n)
+            addition += nums[r];
+            addition -= nums[l];
+            l++;
+            max_average = std::max(max_average, (addition / k)); // k != 0
+        }
+
+        return max_average;
     }
 };
